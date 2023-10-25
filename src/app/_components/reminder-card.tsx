0297dda903022824634fda6f7d21095a52ea54c7
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-25 23:25:05 +0200

    feat: optimistic delete

--- src/app/_components/reminder-card.tsx
+++ src/app/_components/reminder-card.tsx
@@ -13,6 +13,7 @@ import { Button } from "./ui/button";
 import { format } from "date-fns";
 import { api } from "~/trpc/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface IReminder {
   id: number;
@@ -27,14 +28,25 @@ export default function ReminderCard({
 }: {
   reminder: IReminder;
 }) {
+  const [deleted, setDeleted] = useState(false);
   const router = useRouter();
   const deleteReminder = api.reminder.deleteReminderById.useMutation({
     onSuccess: () => {
       router.refresh();
     },
+    onMutate: () => {
+      setDeleted(true);
+    },
   });
+
   return (
-    <Card>
+    <Card
+      style={{
+        transition: "all .8s",
+        opacity: deleted ? "0" : "1",
+        visibility: deleted ? "hidden" : "visible",
+      }}
+    >
       <CardHeader>
         <CardTitle>{name}</CardTitle>
         <CardDescription>{repeatPeriodicity}</CardDescription>
