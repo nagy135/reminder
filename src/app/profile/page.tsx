e25283d7551b85400b09bc6dfba8390b29290e5a
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-25 20:34:31 +0200

    feat: link back to add page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,8 @@ import { currentUser } from "@clerk/nextjs";
 import { api } from "~/trpc/server";
 import ReminderCard from "../_components/reminder-card";
 import { Periodicity } from "~/enums";
+import { Button } from "../_components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   const user = await currentUser();
@@ -11,11 +13,14 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
+      <Button variant="outline" className="absolute left-0 top-0 m-3">
+        <Link href="/">Add reminder</Link>
+      </Button>
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Your reminders
         </h1>
-        <div className="m-5 w-1/2">
+        <div className="m-5 flex w-full flex-col gap-5 md:w-1/2">
           {reminders.map((e) => (
             <ReminderCard
               key={e.id}
