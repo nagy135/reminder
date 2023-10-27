65dce9309fb227d8c7159e3284f357b8ed4145a5
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-27 13:55:56 +0200

    feat: your reminders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
       <Button variant="outline" className="absolute left-0 top-0 m-3">
-        <Link href="/profile">My reminders</Link>
+        <Link href="/profile">Your reminders</Link>
       </Button>
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
