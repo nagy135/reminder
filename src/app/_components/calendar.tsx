9bb67ad08a15cba5cab5fed2340f18c20ff86097
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-11-05 20:07:31 +0100

    feat: show reminder dates

--- src/app/_components/calendar.tsx
+++ src/app/_components/calendar.tsx
@@ -1,15 +1,20 @@
 "use client";
 
+import { useState } from "react";
 import { Calendar as ShadcnCalendar } from "../_components/ui/calendar";
 import useScreenWidth from "../hooks/use-screen-width";
+import { Reminder } from "~/types";
 
-export default function Calendar() {
+export default function Calendar({ reminders }: { reminders: Reminder[] }) {
   const windowSize = useScreenWidth();
+  const [days] = useState<Date[] | undefined>(reminders.map((e) => e.remindAt));
+
   return (
     <ShadcnCalendar
-      mode="single"
+      mode="multiple"
       numberOfMonths={windowSize !== null && windowSize > 400 ? 3 : 1}
-      selected={new Date()}
+      selected={days}
+      onSelect={undefined}
       // onSelect={(e) => console.log(e)}
       // disabled={(date) => date < new Date()}
     />
--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,9 +1,17 @@
+import { currentUser } from "@clerk/nextjs";
 import Calendar from "../_components/calendar";
+import { api } from "~/trpc/server";
+import { Reminder } from "~/types";
 
-export default function CalendarPage() {
+export default async function CalendarPage() {
+  const user = await currentUser();
+  if (!user) return <div>Not logged in</div>;
+
+  const reminders = await api.reminder.getRemindersByUserId.query(user.id);
+
   return (
     <div className="m-2 flex justify-center pt-[150px]">
-      <Calendar />
+      <Calendar reminders={reminders as Reminder[]} />
     </div>
   );
 }
