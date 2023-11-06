8d63a5751587290eff7e7d4b3ccb9c549ae910af
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-11-06 14:37:05 +0100

    feat: header margin

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -10,11 +10,16 @@ export default async function CalendarPage() {
   const reminders = await api.reminder.getRemindersByUserId.query(user.id);
 
   return (
-    <>
+    <div className="flex min-h-screen flex-col items-center">
       <ProfileNavigation list={true} />
-      <div className="m-2 flex justify-center pt-[150px]">
-        <Calendar reminders={reminders} />
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="mt-5 text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Calendar
+        </h1>
+        <div className="m-5">
+          <Calendar reminders={reminders} />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -11,7 +11,7 @@ export default async function ProfilePage() {
   const reminders = await api.reminder.getRemindersByUserId.query(user.id);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center ">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <ProfileNavigation />
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="mt-5 text-5xl font-extrabold tracking-tight sm:text-[5rem]">
